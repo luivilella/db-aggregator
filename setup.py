45c2719132ed046708dc9913b1b09d30e3939bdf
Luís Vilella <luis.vilella@upessencia.com.br>
2016-02-08 05:35:28 +0000

    package

---
+++ setup.py
@@ -0,0 +1,17 @@
+# coding: utf8
+from setuptools import setup
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
+setup(
+    name='db-aggregator',
+    version='0.1',
+    description='Package helpful for manage database connections',
+    url='https://github.com/luivilella/db-aggregator',
+    author='Luis Eduardo Vilella',
+    author_email='luivilella@gmail.com',
+    license='BSD',
+    install_requires=requirements,
+    zip_safe=False,
+)
